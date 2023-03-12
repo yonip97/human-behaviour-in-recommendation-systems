704bddb22c3cbe62546783ba36ef2bb9d55ad942
yonip97 <70665101+yonip97@users.noreply.github.com>
2023-03-12 23:50:10 +0200

    Added strategies, added top k strategy, and a new way to pass strategy params

--- enviorment.py
+++ enviorment.py
@@ -1,13 +1,17 @@
 from models import DocsRankSVM, BM25, Cosiniesimilaritymodel
 import numpy as np
+from scipy.special import softmax
 
 
 class Agent():
-    def __init__(self, id, string: str, strategic: bool, number_of_word_to_add: int):
+    def __init__(self, id, string: str, strategic: bool, number_of_word_to_add: int, strategy: str, k: int = 1,t:int = 100):
         self.id = id
         self.string = string
         self.strategic = strategic
         self.number_of_word_to_add = number_of_word_to_add
+        self.strategy = strategy
+        self.k = k
+        self.t = t
 
     def create_strategy(self, queries: dict, queries_worth: dict):
         """Creates a strategy for the agent."""
@@ -17,6 +21,14 @@ class Agent():
 
     def add_words(self, queries: dict, queries_worth: dict):
         """Returns the agent's string with the added words."""
+        if self.strategy == "top_k_softmax":
+            self.top_k_softmax(queries, queries_worth)
+        elif self.strategy == "dominant_word_not_present":
+            self.dominant_word_not_present(queries, queries_worth)
+        else:
+            raise ValueError("No such strategy exist")
+
+    def top_k_softmax(self, queries: dict, queries_worth: dict):
         words_worth = {}
         for query_id, query in queries.items():
             word_tf = {}
@@ -27,7 +39,27 @@ class Agent():
             for word in word_tf:
                 if word not in words_worth.keys():
                     words_worth[word] = 0
-                words_worth[word] += np.log2(word_tf[word]) * queries_worth[query_id]
+                words_worth[word] += np.log2(word_tf[word]+1) * queries_worth[query_id]
+        words_worth = [(k, v) for k, v in words_worth.items()]
+        words_worth = sorted(words_worth, reverse=True, key=lambda x: x[1])[:self.k]
+        top_k_words = np.array([k[0] for k in words_worth])
+        top_k_scores_after_temperature = [k[1]/self.t for k in words_worth]
+        top_k_scores_after_temperature = softmax(top_k_scores_after_temperature)
+        words = np.random.choice(top_k_words, size=self.number_of_word_to_add, p=top_k_scores_after_temperature)
+        self.string += " ".join(words)
+
+    def dominant_word_not_present(self, queries: dict, queries_worth: dict):
+        words_worth = {}
+        for query_id, query in queries.items():
+            word_tf = {}
+            for word in query.split():
+                if word not in word_tf:
+                    word_tf[word] = 0
+                word_tf[word] += 1
+            for word in word_tf:
+                if word not in words_worth.keys():
+                    words_worth[word] = 0
+                words_worth[word] += np.log2(word_tf[word]+1) * queries_worth[query_id]
         words_worth = [(k, v) for k, v in words_worth.items()]
         words_worth = sorted(words_worth, reverse=True, key=lambda x: x[1])
         chosen_word = None
@@ -38,10 +70,9 @@ class Agent():
         self.string += " ".join([chosen_word for x in range(self.number_of_word_to_add)])
 
 
-
 class Env():
     def __init__(self, docs: dict, strategic_percentage: float, model_type: str, number_of_word_to_add: int,
-                 train_queries=None, train_relevance_ranking=None,seed = 42):
+                 train_queries=None, train_relevance_ranking=None, seed=42, agents_params = None):
         self.agents = None
         self.docs = docs
         self.strategic_percentage = strategic_percentage
@@ -49,7 +80,7 @@ class Env():
         self.model = self.create_model(model_type, train_queries, train_relevance_ranking)
         self.curr_epoch = 0
         self.rng = np.random.default_rng(seed=seed)
-
+        self.agents_params = agents_params
 
     def change_config(self):
         possible_agents = np.array([id for id in self.docs.keys()])
@@ -62,9 +93,9 @@ class Env():
         agents = {}
         for index in possible_agents:
             if index in strategic_agents:
-                agents[index] = Agent(index, self.docs[index], True, number_of_words_to_add)
+                agents[index] = Agent(index, self.docs[index], True, number_of_words_to_add, **self.agents_params)
             else:
-                agents[index] = Agent(index, self.docs[index], False, number_of_words_to_add)
+                agents[index] = Agent(index, self.docs[index], False, number_of_words_to_add, **self.agents_params)
         return agents
 
     def create_model(self, model_type: str, train_queries, train_relevance_ranking):
